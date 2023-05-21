78636b4ef059371c064ef78c7df112fcff275ae9
jRodriguezIV <j.a.rodrigueziv@gmail.com>
2023-05-21 05:47:31 -0400

    added css change in carousel so that the name's color matches the color of the plant

--- front-end/src/Components/Carousel/CarouselComp.js
+++ front-end/src/Components/Carousel/CarouselComp.js
@@ -1,27 +1,39 @@
 import "./CarouselComp.css";
-import "react-responsive-carousel/lib/styles/carousel.min.css"
-import { Carousel } from 'react-responsive-carousel'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 import { useNavigate } from "react-router-dom";
 
 export default function CarouselComp({ flowerList }) {
-  let navigate = useNavigate()
+  let navigate = useNavigate();
 
   const handleItemClick = (index, item) => {
-    navigate(`/flowers/${item.key}`)
-  }
+    navigate(`/flowers/${item.key}`);
+  };
 
   return (
-    <Carousel autoFocus={true} showThumbs={false} infiniteLoop={true} onClickItem={(index, item) => handleItemClick(index, item)}>
-        {flowerList.map((flower) => (
-            <div key={flower.id}>
-                <img src={flower.image} alt={flower.name} style={{minHeight:"auto"}}/>
-                <p style={{fontSize:"larger"}} className="legend">Name:&nbsp;{flower.name}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`Cost: $${flower.cost}`}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Tap&nbsp;For&nbsp;Info</p>
-
-            </div>
-        ))}
-
+    <Carousel
+      autoFocus={true}
+      showThumbs={false}
+      infiniteLoop={true}
+      onClickItem={(index, item) => handleItemClick(index, item)}
+    >
+      {flowerList.map((flower) => (
+        <div key={flower.id}>
+          <img
+            src={flower.image}
+            alt={flower.name}
+            style={{ minHeight: "auto" }}
+          />
+          <p style={{ fontSize: "larger" }} className="legend">
+            Name:&nbsp;
+            <span style={{ color: flower.color }}>{flower.name}</span>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`Cost: $${flower.cost}`}
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            Tap&nbsp;For&nbsp;Info
+          </p>
+        </div>
+      ))}
     </Carousel>
-
 
     // <section className="carouselContainer">
     //   <div className="carousel">
