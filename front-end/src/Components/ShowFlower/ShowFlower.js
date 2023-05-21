8142dff8f3811d60fb57dcc0f52f64c25bd0d3a7
jRodriguezIV <j.a.rodrigueziv@gmail.com>
2023-05-21 16:57:48 -0400

    added edit button, adjusted css for showFlower page, updated functionality aswell

--- front-end/src/Components/ShowFlower/ShowFlower.js
+++ front-end/src/Components/ShowFlower/ShowFlower.js
@@ -1,9 +1,51 @@
+import { Link } from "react-router-dom";
+import "./ShowFlower.css";
+
 export default function ShowFlower({ flower }) {
+  console.log(flower);
 
+  return (
+    <div key={flower.id} className="showFlowerContainer">
+      <img className="flowerImage" src={flower.image} alt={flower.name} />
 
-    return (
-        <div className="showflowerContainer">
-            {flower.name}
-        </div>
-    )
+      <div className="flowerInfo">
+        <p>
+          <span className="spanLeft">Flower Name:</span>
+          <span className="spanRight">{flower.name}</span>
+        </p>
+        <p>
+          Flower Cost:&nbsp;
+          <span className="spanRight">{`$${flower.cost}`}&nbsp;(each)</span>
+        </p>
+        <p>
+          Scientific Name:&nbsp;
+          <span className="spanRight">{flower.scientific_name}</span>
+        </p>
+        <p>
+          Flower Color:&nbsp;
+          <span className="spanRight" style={{ color: flower.color }}>
+            {flower.color}
+          </span>
+        </p>
+        <p>
+          Sunlight Requirement:&nbsp;
+          <span className="spanRight">{flower.sunlight}</span>
+        </p>
+        <p>
+          Watering Requirement:&nbsp;
+          <span className="spanRight">{flower.watering}</span>
+        </p>
+        <p>
+          Favorite Flower?&nbsp;
+          <span className="spanRight">{flower.is_favorite ? "⭐️" : "❌"}</span>
+        </p>
+        <Link to={`/flowers/${flower.id}/edit`}>
+        <button className="editButton"> 
+            Edit Flower
+        </button>
+        </Link>
+
+      </div>
+    </div>
+  );
 }
