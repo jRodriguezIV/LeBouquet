115d0e3371ad2a11d1ecc3dd6e172f36ef6cebce
jRodriguezIV <j.a.rodrigueziv@gmail.com>
2023-05-20 22:47:56 -0400

    added create route for backend database

--- back-end/controllers/flowerController.js
+++ back-end/controllers/flowerController.js
@@ -1,28 +1,33 @@
 const express = require("express");
-const { getAllFlowers, getFlower } = require("../queries/flowers")
+const { getAllFlowers, getFlower, createFlower } = require("../queries/flowers");
 const flowers = express.Router();
 
 flowers.get("/", async (req, res) => {
-    const allFlowers = await getAllFlowers();
-    if (allFlowers[0]) {
-        res.status(200).json(allFlowers);
-    } else {
-        res.status(500).json({error: "server error"})
-    }
-})
+  const allFlowers = await getAllFlowers();
+  if (allFlowers[0]) {
+    res.status(200).json(allFlowers);
+  } else {
+    res.status(500).json({ error: "server error" });
+  }
+});
 
 flowers.get("/:id", async (req, res) => {
-    const { id } = req.params;
-    const flower = await getFlower(id);
-    if(flower.id) {
-        res.json(flower)
-    } else {
-        res.status(404).json({error: "flower not found"})
-    }
-})
+  const { id } = req.params;
+  const flower = await getFlower(id);
+  if (flower.id) {
+    res.json(flower);
+  } else {
+    res.status(404).json({ error: "flower not found" });
+  }
+});
 
-
-
-
+flowers.post("/", async (req, res) => {
+  try {
+    const flower = await createFlower(req.body);
+    res.status(200).json(flower);
+  } catch (error) {
+    res.status(400).json({ error: error });
+  }
+});
 
 module.exports = flowers;
--- back-end/queries/flowers.js
+++ back-end/queries/flowers.js
@@ -1,24 +1,37 @@
 const db = require("../db/dbConfig");
 
 const getAllFlowers = async () => {
-    try {
-        const allFlowers = await db.any("SELECT * FROM flowers");
-        return allFlowers
-    } catch (error) {
-        return error
-    }
-}
+  try {
+    const allFlowers = await db.any("SELECT * FROM flowers");
+    return allFlowers;
+  } catch (error) {
+    return error;
+  }
+};
 
 const getFlower = async (id) => {
-    try {
-        const flower = await db.one("SELECT * FROM flowers WHERE id=$1", id);
-        return flower
-    } catch (error) {
-        return error
-    }
-}
+  try {
+    const flower = await db.one("SELECT * FROM flowers WHERE id=$1", id);
+    return flower;
+  } catch (error) {
+    return error;
+  }
+};
+
+const createFlower = async (flower) => {
+  try {
+    const newFlower = await db.one(
+      "INSTER INTO flowers (name, scientific_name, cost, color, watering, sunlight, image) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+      [flower.name, flower.scientific_name, flower.cost, flower.color, flower.watering, flower.sunlight, flower.image]
+    );
+    return newFlower
+  } catch (error) {
+    return error;
+  }
+};
 
 module.exports = {
-    getAllFlowers,
-    getFlower
-}
+  getAllFlowers,
+  getFlower,
+  createFlower
+};
