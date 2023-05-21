7c9fcf60e8fccb74db1f8a037d1952788f44cae9
jRodriguezIV <j.a.rodrigueziv@gmail.com>
2023-05-21 04:12:12 -0400

    aded Carousel, adjusted images, added styling, installed react-responsive-carousel package

--- front-end/src/Pages/Index/Index.js
+++ front-end/src/Pages/Index/Index.js
@@ -1,8 +1,23 @@
-export default function Index () {
+import { useEffect, useState } from "react";
+import Carousel from "../../Components/NavBar/Carousel/Carousel";
+import axios from "axios";
 
-    return (
-        <div>
-            <h1>Index Page</h1>
-        </div>
-    )
+const API = process.env.REACT_APP_API_URL;
+
+export default function Index() {
+  const [flowerList, setFlowerList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${API}/flowers`)
+      .then((response) => setFlowerList(response.data))
+      .catch((e) => console.warn("catch", e));
+  }, []);
+
+  return (
+    <div>
+      <Carousel flowerList={flowerList}/>
+      <h1>Index Page</h1>
+    </div>
+  );
 }
